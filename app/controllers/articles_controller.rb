57d9351ae58a69b17123376bce21450bebf72379
Teshager <teshager8922@gmail.com>
2020-06-23 10:28:33 -0600

    adds comment feature to the article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,6 +7,8 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id])
+    @comment = Comment.new
+    @comment.article_id = @article.id
   end
 
   def new
@@ -19,7 +21,6 @@ class ArticlesController < ApplicationController
     # @article.body = params[:article][:body]
     # @article.save
     # redirect_to article_path(@article)
-
 
     # @article = Article.new(params[:article])
     @article = Article.new(article_params)
@@ -44,8 +45,7 @@ class ArticlesController < ApplicationController
     @article.update(article_params)
 
     flash.notice = "Article '#{@article.title}' Updated!"
-  
+
     redirect_to article_path(@article)
   end
 end
-
